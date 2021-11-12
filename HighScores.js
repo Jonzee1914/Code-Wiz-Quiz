3e91e14ee4820e7149774611a11e8d10316f8468
Erik Jones <jonzee1914@gmail.com>
2021-11-11 22:31:35 -0500

    updated highscore js

--- HighScores.js
+++ HighScores.js
@@ -2,11 +2,6 @@ var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
 
-// Event listener to clear scores 
-clear.addEventListener("click", function () {
-    localStorage.clear();
-    location.reload();
-});
 // Retreives local stroage 
 var allScores = localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
@@ -21,7 +16,14 @@ if (allScores !== null) {
 
     }
 }
-// Event listener to move to index page
+
+// Event listener to clear 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+// Event listener to return to quiz
 goBack.addEventListener("click", function () {
     window.location.replace("./index.html");
 });
